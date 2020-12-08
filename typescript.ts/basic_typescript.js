19e092ba5e758e1546c57370accce00f816b0047
taty500summers <tatiana.bernon@gmail.com>
2020-12-08 15:55:32 +1300

    Testing tranpiler

---
+++ typescript.ts/basic_typescript.js
@@ -0,0 +1,6 @@
+var name = 'Dinanath';
+function printNumsAndStrings(statement) {
+    console.log("\u2139\uFE0F LOG:: " + statement);
+}
+printNumsAndStrings('hello!');
+printNumsAndStrings(10);
--- typescript.ts/basic_typescript.ts
+++ typescript.ts/basic_typescript.ts
@@ -1,3 +1,5 @@
+let name:string = 'Dinanath';
+
 function printNumsAndStrings(statement: string | number  ) {
   console.log(`ℹ️ LOG:: ${statement}`);
 }
